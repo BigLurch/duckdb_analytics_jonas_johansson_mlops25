5e32a2da457ab8efd015bf89c5410b1c4bf608b2
Jonas Johansson <jonas.lg.johansson@gmail.com>
2025-11-10 15:13:40 +0100

    updated queries

---
+++ code_alongs/03_sql_intro/queries.sql
@@ -0,0 +1,18 @@
+SELECT * FROM funny_jokes;
+
+-- sort by rating
+SELECT 
+    * 
+FROM 
+    funny_jokes
+ORDER BY
+    rating;
+
+-- sort by rating descending order
+SELECT 
+    * 
+FROM 
+    funny_jokes
+ORDER BY
+    rating
+DESC;
